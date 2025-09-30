a49867b21617096123cc4fa0e380651eca2c24c9
guff192 <kochkons1@ya.ru>
2025-10-01 00:59:18 +0400

    :wrench: Change names for Postgres-related settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from pydantic import PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -13,15 +14,27 @@ class Settings(BaseSettings):
 
     REDIS_URL: str
 
-    DATABASE_HOSTNAME: str
-    DATABASE_PORT: str
-    DATABASE_PASSWORD: str
-    DATABASE_NAME: str
-    DATABASE_USERNAME: str
+    POSTGRES_HOSTNAME: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
 
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URL(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USERNAME,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOSTNAME,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+    # SECRET_KEY: str
+    # ALGORITHM: str
+    # ACCESS_TOKEN_EXPIRE_MINUTES: int
 
 
 settings = Settings()  # type: ignore
