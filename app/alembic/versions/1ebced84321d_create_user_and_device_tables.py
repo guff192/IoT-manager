00344409a91b479e7d2099e066f627d3559f33e0
guff192 <kochkons1@ya.ru>
2025-10-04 01:28:40 +0400

    :card_file_box: Configure alembic and add first migration

---
+++ app/alembic/versions/1ebced84321d_create_user_and_device_tables.py
@@ -0,0 +1,73 @@
+"""Create user and device tables
+
+Revision ID: 1ebced84321d
+Revises: 
+Create Date: 2025-10-03 21:12:28.778839
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
+
+
+# revision identifiers, used by Alembic.
+revision: str = '1ebced84321d'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+
+    op.create_table(
+        "user",
+        sa.Column("email", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.Column("full_name", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column(
+            "hashed_password", sqlmodel.sql.sqltypes.AutoString(), nullable=False
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index("ix_user_email", "user", ["email"], unique=True)
+
+    op.create_table(
+        "device_type",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+    op.create_table(
+        "device",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("device_type_id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["device_type_id"],
+            ["device_type.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["user.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_device_name"), "device", ["name"])
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+
+    op.drop_table("device")
+    op.drop_index(op.f("ix_device_name"), table_name="device")
+    op.drop_table("device_type")
+    op.drop_index("ix_user_email", table_name="user")
+    op.drop_table("user")
+
