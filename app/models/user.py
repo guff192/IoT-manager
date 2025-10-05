e7aacb8195c45e75bee58fd09d1741d3225a4fec
guff192 <kochkons1@ya.ru>
2025-10-05 19:33:04 +0400

    :label: Add user input types (for API)

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,22 @@ class UserBase(SQLModel):
     full_name: str | None = Field(default=None, max_length=255)
 
 
+class UserCreate(UserBase):
+    password: str
+
+
+class UserRegister(SQLModel):
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=40)
+    full_name: str | None = Field(default=None, max_length=255)
+
+
+# Properties to receive via API on update, all are optional
+class UserUpdate(UserBase):
+    email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
+    password: str | None = Field(default=None, min_length=8, max_length=40)
+
+
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
