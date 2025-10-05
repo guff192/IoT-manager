f1a0394a06f49d243b4850c4236ca7b1312431b2
guff192 <kochkons1@ya.ru>
2025-10-05 19:55:53 +0400

    :label: Added new types for fastapi's DI mechanism

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,11 +1,23 @@
 from collections.abc import AsyncGenerator
 from typing import Annotated
 
+import jwt
 from fastapi import Depends, Request
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.api.exceptions.server import ErrServiceUnavailable
+from app.api.exceptions.user import ErrNotEnoughPrivileges, ErrUnauthorized, ErrUserNotFound
+from app.core import security
+from app.core.config import settings
+from app.models import TokenPayload
+from app.models import User
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+)
 
 
 async def get_async_engine(request: Request) -> AsyncEngine:
@@ -23,3 +35,29 @@ async def get_async_db(
 
 
 AsyncSessionDep = Annotated[AsyncSession, Depends(get_async_db)]
+TokenDep = Annotated[str, Depends(reusable_oauth2)]
+
+
+async def get_current_user(session: AsyncSessionDep, token: TokenDep) -> User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (jwt.InvalidTokenError, ValidationError):
+        raise ErrUnauthorized(detail="Could not validate credentials")
+
+    user = await session.get(User, token_data.sub)
+    if not user:
+        raise ErrUserNotFound
+
+    return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+async def get_current_active_superuser(current_user: CurrentUser) -> User:
+    if not current_user.is_superuser:
+        raise ErrNotEnoughPrivileges
+    return current_user
