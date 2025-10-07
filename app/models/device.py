fe1f2b679bb121c4481475f865f090624339999b
guff192 <kochkons1@ya.ru>
2025-10-07 21:33:14 +0400

    :label: Add device-related API types

--- app/models/device.py
+++ app/models/device.py
@@ -11,14 +11,28 @@ class DeviceTypeBase(SQLModel):
     """Scheme for device type"""
 
     name: str = Field(
+        unique=True,
         index=True,
         description="A type of device (e.g. 'Light', 'Humidity', 'Thermostat')",
     )
 
 
+class DeviceTypeCreate(DeviceTypeBase):
+    pass
+
+
 class DeviceType(DeviceTypeBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     devices: list["Device"] = Relationship(back_populates="type")
+
+
+class DeviceTypePublic(DeviceTypeBase):
+    id: int
+
+
+class DeviceTypesPublic(SQLModel):
+    data: list[DeviceTypePublic]
+    count: int
 
 
 class DeviceBase(SQLModel):
@@ -28,6 +42,16 @@ class DeviceBase(SQLModel):
         index=True, description="User name for device (e.g. 'Living room light')"
     )
     is_active: bool = Field(default=False, description="Current state of activity")
+
+
+class DeviceCreate(DeviceBase):
+    type_id: int
+
+
+class DeviceUpdate(DeviceBase):
+    name: str | None = Field(default=None)  # type: ignore
+    is_active: bool | None = Field(default=None)  # type: ignore
+    type_id: int | None = Field(default=None)
 
 
 class Device(DeviceBase, table=True):
@@ -40,3 +64,12 @@ class Device(DeviceBase, table=True):
         foreign_key="user.id", nullable=False, ondelete="CASCADE"
     )
     user: Optional["User"] = Relationship(back_populates="devices")
+
+
+class DevicePublic(DeviceBase):
+    id: uuid.UUID
+
+
+class DevicesPublic(SQLModel):
+    data: list[DevicePublic]
+    count: int
