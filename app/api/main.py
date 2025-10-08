025f0eb6cf47650c375f75d8af797e2b1ca958a1
guff192 <kochkons1@ya.ru>
2025-10-08 21:50:52 +0400

    :bug: Add missing sensors router inclusion to main api router

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import devices, login, users, utils
+from app.api.routes import devices, login, sensors, users, utils
 
 api_router = APIRouter()
 api_router.include_router(utils.router)
 api_router.include_router(users.router)
 api_router.include_router(login.router)
 api_router.include_router(devices.router)
+api_router.include_router(sensors.router)
