1e0318ea6e9b9abf500b141397a0d0551efaf1c2
guff192 <kochkons1@ya.ru>
2026-05-25 19:15:36 +0400

    :wrench: Add dev dependencies and update test session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 from typing import AsyncGenerator
 import pytest_asyncio
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from app.core.db import async_engine
+from app.core.config import settings
 
 
 @pytest_asyncio.fixture(scope="function", autouse=True)
 async def session() -> AsyncGenerator[AsyncSession]:
-    async with AsyncSession(async_engine) as session:
+    test_engine = create_async_engine(
+        "postgresql+asyncpg://user:password@localhost:5432/db"
+    )
+    async with AsyncSession(test_engine) as session:
         yield session
